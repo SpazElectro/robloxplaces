7fbd7ff0f597080293ed89f7a0065a582c02c252
StevenRafft <steven.rafft@gmail.com>
2024-05-23 13:26:44 +0300

    allow glitch glitch to work

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask
-
+from flask import Flask, request
 import bs4, requests
 
 app = Flask(__name__)
 
-@app.get("/<game_id>")
-def get_universe(game_id):
-    if game_id == None:
+@app.route("/")
+def get_universe():
+    game_id = request.args.get("game_id")
+    
+    if game_id == None or (isinstance(game_id, str) and not game_id.isdigit()):
         game_id = 12519560096
-
     game_id = str(game_id)
-    req = requests.get(f"https://www.roblox.com/games/{game_id}")
+    req = requests.get(f"https://www.roblox.com/games/{str(game_id)}")
 
     soup = bs4.BeautifulSoup(req.text, "html.parser")
     metadata = soup.find(id="game-detail-meta-data")
@@ -20,3 +20,6 @@ def get_universe(game_id):
         "universe_id": universe_id,
         "places": requests.get(f"https://develop.roblox.com/v1/universes/{str(universe_id)}/places?isUniverseCreation=false&limit=100&sortOrder=Asc").json()["data"]
     }
+
+if __name__ == "__main__":
+    app.run()
